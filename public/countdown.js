7baf384b3fef32aa99ad8453e13366dc801563e4
Chris Williams <clmw83@gmail.com>
2018-08-26 16:52:27 -0400

    Added countdown timer code

---
+++ public/countdown.js
@@ -0,0 +1,27 @@
+function countdown(){
+  var dfr = new $.Deferred();
+  $('#overlay').css("display","block");
+  setTimeout(function() {
+    var time = 5; /* how long the timer will run (seconds) */
+    $('h2').text(time);
+    var initialOffset = '440';
+    var i = 1;
+    /* Need initial run as interval hasn't yet occured... */
+    $('.circle_animation').css('stroke-dashoffset', -1*(initialOffset/time));
+
+      var interval = setInterval(function() {
+          $('h2').text(time-i);
+          if (i == time) {
+            clearInterval(interval);
+            console.log("resolved");
+            $('#overlay').css("display","none");
+            // DO stuff here
+
+            return;
+          }
+          $('.circle_animation').css('stroke-dashoffset', -1*((i+1)*(initialOffset/time)));
+          i++;
+    }, 1000);
+  }, 0);
+  return dfr.promise();
+}
